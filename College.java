97e26cb35e402c1e13abe3901c00b0ccf1c2e0b5
sakshiw131-hub <sakshiw131@gmail.com>
2026-03-24 22:14:24 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,19 @@
+
+public class College {
+	
+	String collegenm;
+	int totalStudent;
+	 
+	void openCollege()  {
+		
+		System.out.println("The college has open");
+		
+	}
+	void closeCollege() {
+		
+		System.out.println("The college has closed");
+	
+	}
+	
+
+}
